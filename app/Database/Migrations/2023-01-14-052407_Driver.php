c9b39fc64e7747d187fe3dad19b2daff4d9f3b02
AgungRamadhani26 <agungramadhani@students.undip.ac.id>
2023-02-07 17:34:55 +0700

    bersih

--- app/Database/Migrations/2023-01-14-052407_Driver.php
+++ app/Database/Migrations/2023-01-14-052407_Driver.php
@@ -20,6 +20,18 @@ class Driver extends Migration
                 'type'       => 'VARCHAR',
                 'constraint' => '50',
             ],
+            'created_at' => [
+                'type'  => 'DATETIME',
+                'null'  => true,
+            ],
+            'updated_at' => [
+                'type'  => 'DATETIME',
+                'null'  => true,
+            ],
+            'deleted_at' => [
+                'type'  => 'DATETIME',
+                'null'  => true,
+            ]
         ]);
         $this->forge->addKey('id_driver', true);
         $this->forge->createTable('driver');
--- app/Models/DriverModel.php
+++ app/Models/DriverModel.php
@@ -8,6 +8,8 @@ class DriverModel extends Model
 {
     protected $table = 'driver'; //artinya model ini menggunakan tabel driver dari database
     protected $primaryKey = 'id_driver'; //primary keynya
+    protected $useTimestamps = true; //menggunakan timestaps agar ketika input masuk ke database waktu yang tercatat adalah waltu saat itu, dan ketika melakukan edit waktu yang tercatat adalah waktu edit
+    protected $useSoftDeletes = true; //menggunakan soft deletes milik CI4, caranya dengan menambahkan kolom pada tabel yang bernama deleted_at
     protected $allowedFields = ['nama']; //berguna untuk mengijinkan kolom mana saja yg dapat kita isi secara manual melalui aplikasi yang kita buat
 
     //Fungsi buatan yang berguna untuk mendapatkan data driver
